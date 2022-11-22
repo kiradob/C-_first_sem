816f052af9d14f4d14f6cd6831cb86658fea7487
kira.dob <arik.dobrynina@yandex.ru>
2022-11-22 15:15:10 +0700

    added task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -2,9 +2,18 @@
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 //2 -> " -2, -1, 0, 1, 2"
 
-Console.Write("Ввести число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-for (int i=-N;i<N+1; i++)
+// Console.Write("Ввести число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int i=-N;i<N+1; i++)
+// {
+//     Console.Write(i);
+// }
+
+Console.WriteLine("Ввести число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while(count<=number)
 {
-    Console.Write(i);
+    Console.WriteLine(count);
+    count++;
 }
