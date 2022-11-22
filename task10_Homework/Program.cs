fe1784c31173f88147d84e66f04daaeca5298262
kira.dob <arik.dobrynina@yandex.ru>
2022-11-22 20:35:52 +0700

    added task10 Homework

---
+++ task10_Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Ввести трёхзначное число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+string numberText = Convert.ToString(number);
+Console.WriteLine($"вторая цифра числа {number} -> " + numberText[1]);
+
