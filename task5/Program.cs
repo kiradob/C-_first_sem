9cf0de5a18cfc6d86970558d3e125bf8a4d4942f
kira.dob <arik.dobrynina@yandex.ru>
2022-11-22 14:58:48 +0700

    added task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -2,4 +2,9 @@
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 //2 -> " -2, -1, 0, 1, 2"
 
-
+Console.Write("Ввести число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i=-N;i<N+1; i++)
+{
+    Console.Write(i);
+}
