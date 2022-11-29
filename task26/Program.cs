cc35bab3930a4219301d0e11c8096e66330519ed
kira.dob <arik.dobrynina@yandex.ru>
2022-11-29 23:20:36 +0700

    added task26

---
+++ task26/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int CountNumber(int num)
+{
+int count1 = 0;
+for (int count = 0; num != 0; count++)
+{
+num /=10;
+count1++;
+}
+return count1;
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = CountNumber(num);
+Console.WriteLine(result);
