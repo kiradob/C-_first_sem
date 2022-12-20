62859c188846ebea8042a416475a002982a5f3fa
kira.dob <arik.dobrynina@yandex.ru>
2022-12-20 13:54:51 +0700

    added task 39

---
+++ task39/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет
+// на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] FillArray(int size)
+{
+    int[] arr=new int[size];
+    for(int i=0; i<arr.Length; i++)
+    {
+        arr[i]=new Random().Next(-9,10);
+    }
+    return arr;
+}
+
+void ReversArry(int[] array)
+{
+    for(int i=0;i<array.Length/2; i++)
+    {
+        int temp=array[i];
+        array[i]=array[array.Length-i-1];
+        array[array.Length-i-1]=temp;
+    }
+}
+
+Console.Write("Введите длину массива: ");
+int num=Convert.ToInt32(Console.ReadLine());
+int[] array=FillArray(num);
+Console.WriteLine(string.Join(",", array));
+ReversArry(array);
+Console.WriteLine(string.Join(",", array));
