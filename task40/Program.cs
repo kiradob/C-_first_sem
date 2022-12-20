719c9a8aaa75dcce2c10790f9c851f852794d764
kira.dob <arik.dobrynina@yandex.ru>
2022-12-20 15:15:36 +0700

    added task 40

--- task40/Program.cs
+++ task40/Program.cs
@@ -2,10 +2,11 @@
 // может ли существовать треугольник с сторонами такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
 
-Console.WriteLine("Введите последовательно три числа: ");
-
+Console.WriteLine("Введите сторону А: ");
 int A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B: ");
 int B=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C: ");
 int C=Convert.ToInt32(Console.ReadLine());
 
 if (A+B>C && B+C>A && C+A>B)
