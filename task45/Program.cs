d3d4f31abb612cefe495b843568c240c6b97df4f
kira.dob <arik.dobrynina@yandex.ru>
2022-12-20 16:31:11 +0700

    added task 45

---
+++ task45/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с 
+// помощью поэлементного копирования.
+
+int[] FillArray(int size)
+{
+
+int[] arr = new int[size];
+for(int i = 0; i < arr.Length; i++)
+{
+arr[i] = new Random().Next(-9,10);
+}
+return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(string.Join(", ", array));
+
+int[] newArray = new int[num];
+for(int i = 0; i < array.Length; i++)
+{
+newArray[i] = array[i];
+}
+Console.WriteLine(string.Join(", ", newArray));
