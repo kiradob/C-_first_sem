d28cfe8f4027be91c38a5ce413eb92ef9dc2f8a1
kira.dob <arik.dobrynina@yandex.ru>
2022-12-22 14:24:59 +0700

    added task 48

---
+++ task48/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+            {
+                matrix[i,j] = i+j;
+                Console.Write(matrix[i,j] + " ");
+            }
+        Console.WriteLine();
+    }
