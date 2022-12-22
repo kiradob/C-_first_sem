8d0f66bd44e1423289f62d3ea6bd382acdc94d3d
kira.dob <arik.dobrynina@yandex.ru>
2022-12-22 11:58:49 +0700

    added task 46

--- task46/Program.cs
+++ task46/Program.cs
@@ -4,3 +4,20 @@
 // 1 4 8 19
 // 5 -2 33 -2
 // 77 3 8 1
+
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+            {
+                matrix[i,j] = new Random().Next(0,11);
+                Console.Write(matrix[i,j] + " ");
+            }
+        Console.WriteLine();
+    }
