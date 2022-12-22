bf52bd5151cb711ea0701ad964dc020102cf41c5
kira.dob <arik.dobrynina@yandex.ru>
2022-12-22 15:40:52 +0700

    added task 52 Нomework

---
+++ task52_Homework/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+// элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите размерность массива m x n");
+Console.Write("Введите количество строк m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// Создание матрицы
+int[,] matrix = new int[m, n];
+// Объявление метода ArrayRandomNumbers
+ArrayRandomNumbers(matrix);
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        avarage = (avarage + matrix[i, j]);
+    }
+    
+    avarage = avarage / m;
+    Console.Write($"Ср. арифм. столбца {j+1}: ");
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(matrix);
+
+void ArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+// Метод PrintArray - представляет полученную мартицу в терминале
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
