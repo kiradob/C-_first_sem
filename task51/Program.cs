99357d8d9eb541c6f70e6771d4cff8ed138ca441
kira.dob <arik.dobrynina@yandex.ru>
2022-12-22 14:33:22 +0700

    added task 51

---
+++ task51/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int [,] matrix = new int [m,n];
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(0,11);
+        if(i == j)
+            {
+                sum+=matrix[i,j];
+            }
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(sum);
