e49e8d16db05b0cf123ce8e33ec4c5148a73c36d
kira.dob <arik.dobrynina@yandex.ru>
2022-12-27 21:41:05 +0700

    added task 54 Нomework

---
+++ task54_Homework/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+Console.WriteLine("Введите размерность массива m x n");
+Console.Write("Введите количество строк m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m,n];
+ArrayRandomNumbers(matrix);
+Console.WriteLine();
+Console.WriteLine("Массив до изменения");
+PrintArray(matrix);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+            if (matrix[i, k] < matrix[i, k + 1])
+            {
+                int temp = 0;
+                temp = matrix[i, k];
+                matrix[i, k] = matrix[i, k + 1];
+                matrix[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными по убыванию значениями");
+PrintArray(matrix);
+
+void ArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+// Метод PrintArray - представляет полученную мартицу в терминале
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
