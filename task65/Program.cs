9404d0e767450e31450f3ca8a9d5522d40db5c06
kira.dob <arik.dobrynina@yandex.ru>
2022-12-28 23:11:33 +0700

    added task 65

---
+++ task65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void Rec(int n, int m)
+{
+if (m > n)
+{
+if (n == m)
+{
+return;
+}
+Rec(n, m - 1);
+Console.Write($"{m} ");
+}
+else if (m<n)
+{
+if (m == n) return;
+Rec(m, n - 1);
+Console.Write($"{n} ");
+}
+else Console.Write($"{m} ");
+}
+Console.Write("Введите первое значение: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите второе значение: ");
+int M = int.Parse(Console.ReadLine());
+Rec(N, M);
