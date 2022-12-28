fac7648b6f968da4f733661b5bc3fc45eb3259f6
kira.dob <arik.dobrynina@yandex.ru>
2022-12-28 22:25:25 +0700

    added task 63

--- task63/Program.cs
+++ task63/Program.cs
@@ -3,6 +3,21 @@
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
 
+// int count=0;
+// void Numbers (int num)
+// {
+//     if (num==0)
+//     {
+//         return;
+//     }
+//     count++;
+//     Console.Write($"{count} ");
+//     Numbers(num-1);
+// }
+// Console.Write("Введите число: ");
+// int nums=int.Parse(Console.ReadLine());
+// Numbers(nums);
+
 int count=0;
 void Numbers (int num)
 {
@@ -10,9 +25,8 @@ void Numbers (int num)
     {
         return;
     }
-    count++;
-    Console.Write($"{count} ");
     Numbers(num-1);
+    Console.Write($"{num} ");
 }
 Console.Write("Введите число: ");
 int nums=int.Parse(Console.ReadLine());
